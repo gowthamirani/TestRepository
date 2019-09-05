595a8b0c71c49cbf38e03a88367146544c5db67e
ITFS <ITFS@ITFS-17>
2019-09-05 11:26:43 +0530

    After addingsecond line save it in remote repo

---
+++ GitDemoProject/src/com/manipal/Demo.java
@@ -0,0 +1,12 @@
+package com.manipal;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello Git");
+		System.out.println("second line added");
+	}
+
+}
